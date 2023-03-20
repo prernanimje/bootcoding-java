f1f3c56d7160786d0507640b67e95a9c478c123c
prernanimje <prernanimje5@gmail.com>
2023-03-20 18:44:27 +0530

    Abstract Example

---
+++ src/main/java/classWork/abstractExample.java
@@ -0,0 +1,27 @@
+package classWork;
+
+public class abstractExample {
+    void show(){
+        int a=10;
+        System.out.println(a);
+    }
+}
+abstract class Class1{
+    void show1(){
+        int s=1204;
+        int t=s-10;
+        System.out.println(t);
+    }
+}
+class Class2 extends Class1{
+    void show2(){
+        System.out.println("Hello");
+    }
+}
+class demo3{
+    public static void main(String[] args) {
+       // Class1 c1= new Class1() {
+
+        }
+    }
+
