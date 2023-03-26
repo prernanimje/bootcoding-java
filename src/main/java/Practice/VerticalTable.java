1ebab11e4a35ea6840abbcbea2d44f766849f059
prernanimje <prernanimje5@gmail.com>
2023-03-26 22:43:01 +0530

    Table print

---
+++ src/main/java/Practice/VerticalTable.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class VerticalTable {
+    public static void main(String[] args) {
+        int a = 2;
+        int multi;
+
+        for (int i=1;i<=10;i++){
+
+               multi = a * i*1;
+               System.out.println("Table of" + a + "is" + multi);
+
+        }
+    }
+}
